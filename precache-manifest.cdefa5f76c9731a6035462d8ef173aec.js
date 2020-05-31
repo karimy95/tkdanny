671d99cf4dc4b7abcf4c4cf1ce97ee755e9bc10c
Karim Youssef <karim.youssef@outlook.com>
2020-05-31 09:18:20 -0400

    Updates

---
+++ precache-manifest.cdefa5f76c9731a6035462d8ef173aec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9ee7907c314411434557332a3706345",
+    "url": "/tkdanny/index.html"
+  },
+  {
+    "revision": "150de51de9ef20e7186e",
+    "url": "/tkdanny/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a0f029581a2954ff037f",
+    "url": "/tkdanny/static/js/2.0f10a6d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tkdanny/static/js/2.0f10a6d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "150de51de9ef20e7186e",
+    "url": "/tkdanny/static/js/main.19974658.chunk.js"
+  },
+  {
+    "revision": "2157a1c478682491f638",
+    "url": "/tkdanny/static/js/runtime-main.30d5eb89.js"
+  }
+]);
